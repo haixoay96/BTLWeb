440e3693e526d3a97e63d9084096f0f3b1048c8d
Duc Linh <haixoay96@gmail.com>
2016-11-13 23:57:10 +0700

    ddd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 var express = require('express');
 var app = express();
 app.set('view engine', 'ejs');
-http.createServer(app).listen(3000, ()=>{
+http.createServer(app).listen(3000, () => {
     console.log('Server running at Port 3000!');
 });
 app.use('/', express.static('public'));
