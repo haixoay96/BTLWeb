1e5e919918c5c46bd1190546e36e759c52933cbe
Lannt <nguyenthilanuet96@gmail.com>
2016-11-16 20:57:53 +0700

    new commit

--- public/js/faculty.js
+++ public/js/faculty.js
@@ -6,31 +6,24 @@ var mainApp = angular.module('Faculty', ['ngRoute']);
 mainApp.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
         .when('/', {
-            templateUrl: '/faculty/views/home',
-            // controller: 'HomeController'
+            templateUrl: '/faculty/views/home'
         })
         .when('/lecturer', {
-            templateUrl: '/faculty/views/lecturer',
-            // controller: 'lecturerController'
+            templateUrl: '/faculty/views/lecturer'
         })
         .when('/course', {
-            templateUrl: '/faculty/views/course',
-            // controller: 'courseController'
+            templateUrl: '/faculty/views/course'
         })
         .when('/courseprogram', {
-            templateUrl: '/faculty/views/courseprogram',
-            // controller: ''
+            templateUrl: '/faculty/views/courseprogram'
         })
         .when('/students', {
-            templateUrl: '/faculty/views/students',
-            controller: ''
+            templateUrl: '/faculty/views/students'
         })
         .when('/dissertation', {
-            templateUrl: '/faculty/views/dissertation',
-            // controller: 'dissertationController'
+            templateUrl: '/faculty/views/dissertation'
         })
         .when('/register_dissertation', {
-            templateUrl: '/faculty/views/register_dissertation',
-            // controller: 'registerController'
+            templateUrl: '/faculty/views/register_dissertation'
         })
 }]);
