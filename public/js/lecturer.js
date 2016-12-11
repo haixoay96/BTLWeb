c8f912015dfd7bd0878c275bb5c4037fc4527c65
Lannt <nguyenthilanuet96@gmail.com>
2016-12-11 18:26:42 +0700

    edit profile

--- public/js/lecturer.js
+++ public/js/lecturer.js
@@ -1,17 +1,51 @@
-angular.module('admin_lecturer',['ui.router'])
-    .config(function($stateProvider,$urlRouterProvider) {
-        $urlRouterProvider.otherwise('/home');
-        $stateProvider
+var mainApp = angular.module('admin_lecturer', ['ui.router']);
+
+mainApp.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+    $stateProvider
         .state('home', {
             url: '/home',
-            templateUrl: '/admin/lecturer/views/profile_lecturer.html'
+            templateUrl: '/admin/lecturer/views/profile_lecturer.html',
+            controller: 'HomeController'
         })
         .state('research', {
             url: '/research',
-            templateUrl: '/admin/lecturer/views/research.html'
+            templateUrl: '/admin/lecturer/views/research.html',
+            controller: 'researchController'
         })
         .state('field', {
             url: '/field',
-            templateUrl: '/admin/lecturer/views/field.html'
+            templateUrl: '/admin/lecturer/views/field.html',
+            controller: 'fieldController'
         })
 });
+mainApp.controller('HomeController', function ($scope, $location) {
+    $scope.user = {
+        name: "Lê Đình Thanh",
+        id: "",
+        faculty: "Công nghệ thông tin",
+        lab: "lab",
+        mail: ""
+
+    };
+    $scope.edit_research = function (user) {
+        // $scope.modify_research = true;
+        $scope.view_research = true ;
+    };
+    $scope.editField = function () {
+        $location.path = '/field';
+    }
+    $scope.update = function (user) {
+        //$scope.modify_research = false;
+        $scope.view_research = false;
+    }
+});
+mainApp.controller('researchController', function () {
+
+});
+
+mainApp.controller('fieldController', function ($scope) {
+
+
+});
+
