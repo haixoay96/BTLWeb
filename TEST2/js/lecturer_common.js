1a45ce2e8e5a5b138c0319a8ed3daa8842767d2d
smart mouse <smart mouse>
2016-12-15 23:00:39 +0700

    view cua phan giang vien

--- TEST2/js/lecturer_common.js
+++ TEST2/js/lecturer_common.js
@@ -1,29 +1,6 @@
 /**
  * Created by thanh huyền on 15-Nov-16.
  */
-// 'use strict';
-// angular.module('faculty',['ui.router'])
-//     .config(function($stateProvider,$urlRouterProvider) {
-//         $urlRouterProvider.otherwise('/CNTT');
-//         $stateProvider
-//             .state('CNTT', {
-//                 url:'/CNTT',
-//                 templateUrl:'views/CNTT.html'
-//             })
-//             .state('DTVT', {
-//                 url: '/DTVT',
-//                 templateUrl: 'views/DTVT.html'
-//             })
-//             .state('CHKT', {
-//                 url: '/CHKT',
-//                 templateUrl: 'views/CHKT.html'
-//             })
-//             .state('VLKT', {
-//                 url: '/VLKT',
-//                 templateUrl: 'views/VLKT.html'
-//             })
-//     });
-
 'use strict';
 angular.module('lecturer',['ui.router'])
     .config(function($stateProvider,$urlRouterProvider) {
