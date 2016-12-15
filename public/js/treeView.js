9b0b2aa1856b469db43ddc19472061c89d95a1f5
smart mouse <smart mouse>
2016-12-16 01:23:34 +0700

    view linh vuc thay doi

---
+++ public/js/treeView.js
@@ -0,0 +1,20 @@
+﻿// $(document).ready(function(){
+// 	$("img.tree-toggle").click(function(){
+// 		if (this.nextSibling.nextSibling.nextSibling.style.display == "") {
+// 			this.nextSibling.nextSibling.nextSibling.style.display = "none";
+// 			this.src = "images/plus.gif";
+// 		} else {
+// 			this.nextSibling.nextSibling.nextSibling.style.display = "";
+// 			this.src = "images/minus.gif";
+// 		}
+// 	});
+// });
+
+$(document).ready(function(){
+	$("a.tree-toggle1").click(function(){
+		$("li.no-child1").toggle();
+	});
+	$("a.tree-toggle2").click(function(){
+		$("li.no-child2").toggle();
+	});
+});
